ddc459d5400219f92d1cf5c962804ed5b95766fe
mherring11 <m.herring11@yahoo.com>
2022-07-02 11:05:53 -0500

    added questions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Creates a function to generate template for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  <h1 align='center'>${data.projectTitle}</h1>
+  
+  ## Description 
+    ${data.description}
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [Contributing](#contributing)
+  ## Installation
+    ${data.installation}
+  ## Usage
+    ${data.usage}
+  
+  ## Credits
+    ${data.credits}  
+  ## License
+    This application is covered by ${data.license} license.
+  ## Features
+    ${data.features}
+  ## Contributing
+    ${data.contributing}
 `;
 }
 
